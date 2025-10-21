cbc283029508bee6d6c3432cff8e1b045bc27a41
Azingamot <159550570+Azingamot@users.noreply.github.com>
2025-10-21 12:15:35 +0500

    Исправлены ошибки

--- app/src/main/java/com/example/calcpractice/MainActivity.java
+++ app/src/main/java/com/example/calcpractice/MainActivity.java
@@ -14,6 +14,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
     private String prevNumber = "";
     private String curNumber = "";
@@ -39,15 +41,27 @@ public class MainActivity extends AppCompatActivity {
     public void EnterNumber(View v)
     {
         TextView text = (TextView)v;
-        curNumber += text.getText();
+        if (curNumber.length() < 8)
+        {
+            if (Objects.equals(curNumber, "0"))
+            {
+                curNumber = "";
+            }
+            curNumber += text.getText();
+        }
+        else
+        {
+            Toast.makeText(this, "Слишком большое число", Toast.LENGTH_SHORT).show();
+        }
+
         ReloadText();
     }
 
     public void MakeOperation(View v)
     {
-        if (curNumber != "")
+        if (!Objects.equals(curNumber, ""))
         {
-            if (curOperation != "") DoCalculation(null);
+            if (!Objects.equals(curOperation, "")) DoCalculation(null);
             TextView text = (TextView)v;
             prevNumber = String.valueOf(curNumber);
             curNumber = "";
@@ -98,7 +112,16 @@ public class MainActivity extends AppCompatActivity {
                     case "*": curNumber = String.valueOf(Integer.parseInt(prevNumber) * Integer.parseInt(curNumber)); break;
                     case "**": curNumber = String.valueOf(Math.round(Math.pow(Integer.parseInt(prevNumber), Integer.parseInt(curNumber)))); break;
                     case "%": curNumber = String.valueOf(Integer.parseInt(prevNumber) % Integer.parseInt(curNumber)); break;
-                    case "/": curNumber = String.valueOf(Math.round(Float.parseFloat(prevNumber) / Float.parseFloat(curNumber))); break;
+                    case "/": if (!Objects.equals(curNumber, "0"))
+                    {
+                        curNumber = String.valueOf(Math.round(Float.parseFloat(prevNumber) / Float.parseFloat(curNumber)));
+                    }
+                    else
+                    {
+                        Toast.makeText(this, "Деление на 0 запрещено", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    break;
                 }
                 prevNumber = "";
                 ReloadText();
